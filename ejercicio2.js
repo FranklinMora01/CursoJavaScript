6cc23d89c62ed3ed2923ff2b84fe4859bce06419
Frankin Rueda <105868794+FranklinMora01@users.noreply.github.com>
2023-08-18 22:57:36 -0400

    se agrega el siclo for para realizar la busqueda sin librerias

--- ejercicio2.js
+++ ejercicio2.js
@@ -2,6 +2,8 @@
 
 Recuerda que la media de tres números se calcula sumando los tres y dividiendo entre 3. */
 
+let vecNumeros = [];
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
@@ -9,8 +11,6 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
-let vecNumeros = [];
 rl.question(`Ingrese el numero 1: `, (num1) => {   
   vecNumeros.push(num1);
   rl.question(`Ingrese el numero 2: `, (num2) => {   
@@ -18,7 +18,11 @@ rl.question(`Ingrese el numero 1: `, (num1) => {
     rl.question(`Ingrese el numero 3: `, (num3) => {   
       vecNumeros.push(num3);
       //de la manera mas facil aplicando metodo
-      const mayor = Math.max(...vecNumeros);
+      //const mayor = Math.max(...vecNumeros);
+      let mayor=0;
+      for(i=0; i<vecNumeros.length; i++){
+        vecNumeros[i] > mayor ? mayor=vecNumeros[i] : mayor=mayor;
+      }    
       console.log('El número mayor es:', mayor);
       rl.close();
     });
